7e48e0018fcf2a5c96e33ac0d9e1f87174516ca8
Finch-H <merchantdaniel8@gmail.com>
2022-10-23 01:20:29 +0000

    little erros

---
+++ lib/Controller/InternationalNews.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:newsportal/View/InternationalNews/In_all_news.dart' as allnews;
+import 'package:newsportal/View/InternationalNews/In_news_gallery.dart' as gallery;
+
+class InternationalNews extends StatefulWidget {
+  @override
+  _InternationalNewsState createState() => new _InternationalNewsState();
+}
+
+class _InternationalNewsState extends State<InternationalNews>with SingleTickerProviderStateMixin {
+
+  TabController ?tabController;
+
+  @override
+  void initState() {
+    tabController=new TabController(length: 2, vsync: this);
+    super.initState();
+  }
+  @override
+  void dispose() {
+    tabController?.dispose();
+    super.dispose();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+
+      appBar: new AppBar(
+        title: new Text("International News"),
+        backgroundColor: Color(0xFF272B4A),
+        bottom: new TabBar(
+          controller: tabController,
+          indicatorColor: Colors.blueGrey,
+          indicatorWeight: 5.0,
+
+          tabs: <Widget>[
+
+            new Tab(icon: Icon(Icons.next_week),text: "International News",),
+            new Tab(icon: Icon(Icons.image),text: "Gallery",)
+
+          ],
+        )
+      ),
+
+      body: new TabBarView(
+        controller: tabController,
+        children: <Widget>[
+
+          new allnews.In_AllNews(),
+          new gallery.In_Gallery()
+        ],
+      ),
+
+    );
+  }
+}
+
+
+
+
+
+
+
---
+++ lib/View/LocalNews/Lo_news_gallery.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class LocalGallery extends StatefulWidget {
+  @override
+  _LocalGalleryState createState() => new _LocalGalleryState();
+}
+
+class _LocalGalleryState extends State<LocalGallery> {
+
+  StreamSubscription<QuerySnapshot>subscription;
+
+  List<DocumentSnapshot>snapshot;
+
+  CollectionReference collectionReference=Firestore.instance.collection("InternationalNews");
+
+  @override
+  void initState() {
+
+    subscription=collectionReference.snapshots().listen((datasnap){
+      setState(() {
+        snapshot=datasnap.documents;
+      });
+    });
+    super.initState();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      backgroundColor: Color(0xFF222240),
+
+      body: new ListView.builder(
+          itemCount: snapshot.length,
+          itemBuilder: (context,index){
+
+            return Container(
+              margin: EdgeInsets.all(10.0),
+              child: new Stack(
+                children: <Widget>[
+
+                  new Container(
+                    child: new ClipRRect(
+                      borderRadius: BorderRadius.circular(15.0),
+                      child: new Image.network(snapshot[index].data["image"],
+                        height: 300.0,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  new Positioned(
+                    top: 40.0,
+                    left: 30.0,
+                    child: new Container(
+                      height: 50.0,
+                      width: 250.0,
+                      margin: EdgeInsets.all(10.0),
+                      decoration: new BoxDecoration(
+                          color: Colors.blueGrey,
+                          borderRadius: BorderRadius.only(
+                              bottomLeft: Radius.circular(15.0),
+                              topRight: Radius.circular(15.0)
+                          )
+                      ),
+                      child: Align(
+                          alignment: Alignment.center,
+                          child: new Text("Local News",
+                            style: TextStyle(
+                                fontSize: 20.0,
+                                color: Colors.white
+                            ),
+                          )
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            );
+
+          }
+      ),
+
+    );
+  }
+
+}
